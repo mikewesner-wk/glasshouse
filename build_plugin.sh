e5d5b8d6e4b2bb89afa9e5432e5084d70fcb8448
Mike Wesner <mike.wesner@webfilings.com>
2014-01-18 17:12:39 -0600

    This should be more flexible, but im still just using this to move the files around.  Bought Indigo 6, so just a path tweak here.

--- build_plugin.sh
+++ build_plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -fr /Library/Application\ Support/Perceptive\ Automation/Indigo\ 5/Plugins/glasshouse.indigoPlugin
-cp -R glasshouse.indigoPlugin /Library/Application\ Support/Perceptive\ Automation/Indigo\ 5/Plugins/
+rm -fr /Library/Application\ Support/Perceptive\ Automation/Indigo\ 6/Plugins/glasshouse.indigoPlugin
+cp -R glasshouse.indigoPlugin /Library/Application\ Support/Perceptive\ Automation/Indigo\ 6/Plugins/
